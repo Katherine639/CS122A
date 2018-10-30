bce6c823deb3ef8054807605be99e465d0e5349a
Katherine639 <kway001@ucr.edu>
2018-10-30 14:02:25 -0700

    Update david007_kway001_Lab6_Part2.c

--- david007_kway001_Lab6_Part2.c
+++ david007_kway001_Lab6_Part2.c
@@ -8,37 +8,75 @@
 enum DemoStates(init, shift_left, shift_right, wait_left, wait_right) LEDstate;
 
 void transmit_data(unsigned char data)
-{
-	unsigned char i;
-	for (i = 0; i < 8 ; ++i)
-	{
-		// Sets SRCLR to 1 allowing data to be set
-		// Also clears SRCLK in preparation of sending data
-		PORTC = 0x08;
-		// set SER = next bit of data to be sent.
-		PORTC |= ((data >> i) & 0x01);
-		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
-		PORTC |= 0x04;
-	}
-	// set RCLK = 1. Rising edge copies data from the “Shift” register to the “Storage” register
-	PORTC |= 0x02;
-	// clears all lines in preparation of a new transmission
-	PORTC = 0x00;
+{   /*
+    SER = PORTA0;
+    OE = PORTA1;
+    RCLK = PORTA2;
+    SRCLK = PORTA3;
+    SRCLR = PORTA4;
+    */
+   
+    // Ensure DDRA is setup as output
+    DDRC = 0xFF;
+   
+    // When ready to begin transmitting, set SRCLR high and RCLK low.
+    PORTC = SetBit(PORTC, 4, 1);
+    PORTC = SetBit(PORTC, 2, 0);
+   
+    // for each bit of data
+    for (int i = 0; i < 8; i++) {
+        // Set SRCLR to 1 allowing data to be set
+        //SetBit(PORTA, 4, 1);
+        // Also clear SRCLK in preparation of sending data
+        PORTC = SetBit(PORTC, 3, 0);
+        // set SER = next bit of data to be sent.
+        PORTC = SetBit(PORTC, 0, 1); //GetBit(data, i) );
+        // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+        PORTC = SetBit(PORTC, 3, 1);
+    }
+    // end for each bit of data
+   
+    // set RCLK = 1. Rising edge copies data from the "Shift" register to the "Storage" register
+    PORTC = SetBit(PORTC, 2, 1);
+    // clears all lines in preparation of a new transmission
+    PORTC = SetBit(PORTC, 4, 0);
 }
-void transmit_data1(unsigned char data)
-{
-	unsigned char i;
-	for (i = 0; i < 8 ; ++i)
-	{
-		// Sets SRCLR to 1 allowing data to be set
-		// Also clears SRCLK in preparation of sending data
-		PORTD = 0x08;
-		// set SER = next bit of data to be sent.
-		PORTD |= ((data >> i) & 0x01);
-		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
-		PORTD |= 0x04;
+	void transmit_data1(unsigned char data)
+	 {
+    /*
+    SER = PORTA0;
+    OE = PORTA1;
+    RCLK = PORTA2;P
+    SRCLK = PORTA3;
+    SRCLR = PORTA4;
+    */
+   
+    // Ensure DDRA is setup as output
+    DDRD = 0xFF;
+   
+    // When ready to begin transmitting, set SRCLR high and RCLK low.
+    PORTD = SetBit(PORTD, 4, 1);
+    PORTD = SetBit(PORTD, 2, 0);
+   
+    // for each bit of data
+    for (int i = 0; i < 8; i++) {
+        // Set SRCLR to 1 allowing data to be set
+        //SetBit(PORTA, 4, 1);
+        // Also clear SRCLK in preparation of sending data
+        PORTD = SetBit(PORTD, 3, 0);
+        // set SER = next bit of data to be sent.
+        PORTD = SetBit(PORTD, 0, 1);//GetBit(data, i) );
+        // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+        PORTD = SetBit(PORTD, 3, 1);
+    }
+    // end for each bit of data
+   
+    // set RCLK = 1. Rising edge copies data from the "Shift" register to the "Storage" register
+    PORTD = SetBit(PORTD, 2, 1);
+    // clears all lines in preparation of a new transmission
+    PORTD = SetBit(PORTD, 4, 0);
 	}
-	// set RCLK = 1. Rising edge copies data from the “Shift” register to the “Storage” register
+	// set RCLK = 1. Rising edge copies data from the â€œShiftâ€ register to the â€œStorageâ€ register
 	PORTD |= 0x02;
 	// clears all lines in preparation of a new transmission
 	PORTD = 0x00;
